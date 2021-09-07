33ac34c157a64f33dbdfbff6ebc6e5d246bbe8c4
pasha2305 <pasha.k2305@gmail.com>
2021-09-07 14:23:48 +0300

    add tests for delete method #3

--- src/test/java/SkippListTest.java
+++ src/test/java/SkippListTest.java
@@ -3,6 +3,41 @@ import org.junit.Test;
 
 
 class SkipListTest {
+
+    @Test
+    void delete() {
+        SkippableList<Integer> list = new SkipList<>();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+
+        Assert.assertTrue(list.delete(2));
+    }
+
+    @Test
+    void deleteMissing(){
+        SkippableList<Integer> list = new SkipList<>();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+
+        Assert.assertFalse(list.delete(10));
+    }
+
+    @Test
+    void deleteOneOfMany(){
+        SkippableList<Integer> list = new SkipList<>();
+        list.insert(1);
+        list.insert(2);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+
+        Assert.assertTrue(list.delete(2));
+    }
+
     @Test
     void search(){
         SkippableList<Integer> list = new SkipList<>();
